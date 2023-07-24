d1031d9f43f1daf6369f8da7317f1bbf94777d23
Taiyou3014 <85092981+Taiyou3014@users.noreply.github.com>
2023-07-24 15:50:59 +0900

    nakaharatest1

---
+++ apu_map/ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyCNXwJPuWA-58sBdknVOBh_KsPUeAyqMLA")
+    GeneratedPluginRegistrant.register(with: self)
+
+
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
